e056f5a99688aa0910f082172ae376410e0eb0d8
angus-x <angus.xiao@live.com>
2021-03-05 18:42:55 +0800

    first

---
+++ dist/service-worker.js
@@ -0,0 +1,25 @@
+importScripts("/precache-manifest.4cf1097da8609d7d89df9ec01dce95a2.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+/* eslint-disable no-undef,no-restricted-globals */
+
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
+
+workbox.core.setCacheNameDetails({
+  prefix: "saleor-store-front"
+});
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+workbox.routing.registerRoute(
+  new RegExp("^http.*(?:png|gif|jpg|jpeg|webp|svg)"),
+  new workbox.strategies.NetworkFirst()
+);
+
+workbox.routing.registerNavigationRoute(
+  workbox.precaching.getCacheKeyForURL("/index.html"),
+  {
+    blacklist: [new RegExp("/graphql"), new RegExp("/dashboard")]
+  }
+);
+
